02a430fae15a1074d077e87b807dd61578ae6d03
Alexander 'Aux' Ivanov <aux@hexmode.org>
2012-03-12 15:13:25 +0200

    Working on vCard, basic functionality is ready

--- src/jquery.aux_.vcard.js
+++ src/jquery.aux_.vcard.js
@@ -3,15 +3,37 @@
 		$.aux = {};
 	}
 
-	/*
-	 * {
-	 *   name: 'xxx',
-	 *   value: 'yyy',
-	 *   params: [],
-	 *   group: 'abc'
-	 * }
-	 */
-	function contentLine() {
+	function parameter(/* name, value */) {
+		var self = this;
+
+		var name = '';
+		var value = '';
+
+		if(arguments.length == 1)
+			self.parse(arguments[0]);
+		else if(arguments.length > 1) {
+			name = arguments[0];
+			value = arguments[1];
+		}
+
+		this.parse = function(string) {
+			if($.isPlainObject(string)) {
+				if(string.hasOwnProperty('name')) name = string.name;
+				if(string.hasOwnProperty('value')) value = string.value;
+			}
+			else {
+				var list = string.split('=', 2);
+				name = list[0];
+				if(list.length > 1)
+					value = list[1];
+			}
+		}
+
+		this.getName = function() {return(name);}
+		this.getValue = function() {return(value);}
+	}
+
+	function contentLine(/* name, value, params, group */) {
 		var self = this;
 
 		var group = null;
@@ -20,6 +42,47 @@
 		var value = '';
 
 		var incoming;
+
+		this.getGroup = function() {return(group);}
+		this.getParams = function() {return(params);}
+		this.getName = function() {return(name);}
+		this.getValue = function() {return(value);}
+
+		// TODO No escaping and no folding yet.
+		this.generate = function() {
+			var result = '';
+			if(group instanceof String && group.length > 0)
+				result = group + '.';
+			result += name.toUpperCase();
+			if($.isArray(params) && params.length > 0)
+				result += ';' + params.join(';');
+			result += ':' + value;
+			return(result);
+		}
+
+		// TODO Implement parser.
+		this.parse = function(string) { }
+
+		this.setData = function(data) {
+			if(data.hasOwnProperty('group')) group = data.group;
+			if(data.hasOwnProperty('name')) name = data.name;
+			if(data.hasOwnProperty('params')) params = data.params;
+			if(data.hasOwnProperty('value')) value = data.value;
+			self.processParams();
+		}
+
+		this.processParams = function() {
+			if(params == null)
+				params = [];
+			else if(!$.isArray(params))
+				params = [params];
+			$.map(params, function(item) {
+				if(item instanceof parameter)
+					return(item);
+				else
+					return(new parameter(item));
+			});
+		}
 
 		// We either got vCard string and should parse it or we got object with all data.
 		if(arguments.length == 1) {
@@ -43,42 +106,10 @@
 					break;
 			}
 		}
-
-		this.getGroup = function() { return(group); }
-		this.getParams = function() { return(params); }
-		this.getName = function() { return(name); }
-		this.getValue = function() { return(value); }
-
-		// TODO No escaping and no folding yet.
-		this.generate = function() {
-			var result = '';
-			if(group instanceof String && group.length > 0)
-				result = group + '.';
-			result += name;
-			if($.isArray(params) && params.length > 0)
-				result += ';' + params.join(';');
-			result += value;
-			return(result);
-		}
-
-		this.parse = function(string) {
-
-		}
-
-		this.setData = function(data) {
-			if(data.hasOwnProperty('group')) group = data.group;
-			if(data.hasOwnProperty('name')) name = data.name;
-			if(data.hasOwnProperty('params')) params = data.params;
-			if(data.hasOwnProperty('value')) value = data.value;
-
-			if(params == null)
-				params = [];
-			else if(!$.isArray(params))
-				params = [params];
-		}
 	}
 
-	function vcardInstance(newVersion) {
+	// TODO Process data in constructor.
+	function vcardInstance(newVersion /* data */) {
 		var self = this;
 
 		var version = newVersion;
@@ -93,8 +124,19 @@
 			return(self);
 		}
 
+		this.addLine = function(line) {
+			data.push(line);
+		}
+
 		this.generate = function() {
-			return([header, 'VERSION:' + version].concat(data, [footer, '']).join('\r\n'));
+			return([header, 'VERSION:' + version].concat(
+				$.map(data, function(item) {
+					if(!(item instanceof contentLine))
+						item = new contentLine(item);
+					return(item.generate());
+				}),
+				[footer, '']).join('\r\n')
+			);
 		}
 	}
 
@@ -109,10 +151,12 @@
 			return(self);
 		}
 
-		this.create = function() {
-			var instance = new vcardInstance(version);
+		this.create = function(/* data */) {
+			var instance = arguments.length > 0 ? new vcardInstance(version, arguments[0]) : new vcardInstance(version);
 			return(instance);
 		}
+
+		this.contentLine = contentLine;
 	}
 
 	$.aux.vcard = new vcard();
